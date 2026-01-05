0030667675188b9b8bdd32ad2b348deeae797a6c
Tatsuya <ntatsuya19971104@yahoo.co.jp>
2026-01-05 22:39:04 +0900

    PostgreSQLとEF Coreの初期設定を追加

--- src/Comic.Api/Program.cs
+++ src/Comic.Api/Program.cs
@@ -1,7 +1,10 @@
+using Comic.Api.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(c =>
 {
     // Swaggerに「Bearerトークン入力欄」を出すための定義
